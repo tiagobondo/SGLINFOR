31ef24ca55655b2fd78c1714ce72d7db5039d8a6
Tiago Bondo <118920546+tiagobondo@users.noreply.github.com>
2023-06-15 19:20:53 +0100

    Update materias.php
    atualizei

--- materias.php
+++ materias.php
@@ -32,6 +32,12 @@
     <a href="acerca.php" class="linkNav"> <i class="fa fa-info" id="NavIcon"></i> Acerca </a>
     <a href="#" class="linkNav" onclick="abrirModal()"> <i class="fa fa-computer" id="NavIcon"></i> Dados</a>
     <a href="sair.php" class="linkNav"> <i class="fa fa-outdent" id="NavIcon"></i> Sair </a>
+    <h6 class="dataEstatistica"><?php
+            date_default_timezone_set('Africa/Luanda');
+            $data = date('Y-m-d');
+            echo $data;
+        
+        ?></h6>
     
     <div class="janela-modal" id="janela-modal">
     <div class="modal1">
