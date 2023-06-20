717efba387955c7a67bf015338340e41fbb1a014
Tiago Bondo <118920546+tiagobondo@users.noreply.github.com>
2023-06-20 11:55:35 +0100

    Revert "Atualizei apagarcargos.php"
    This reverts commit 17914fe18549053538b94a809306c1271ef7656f.

--- apagarcargos.php
+++ apagarcargos.php
@@ -12,7 +12,8 @@ $Cod_cargo = filter_input(INPUT_GET, "Cod_cargo",FILTER_SANITIZE_NUMBER_INT);
         $result_cargo->bindParam('Cod_cargo',$Cod_cargo);
         
 
-        if($result_cargo->execute()){
+        if($result_cargos->execute()){
+            header("Location: cargos.php");
             sleep(1);
             $msg="<div class='alert alert-success'>Cargo apagado Com sucesso!</div>";
             header("Location: cargos.php");
