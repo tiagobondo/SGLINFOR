d2d4e3bc00b5d3efecf21cd4c6ffcf51ab7cfb6a
Tiago Bondo <118920546+tiagobondo@users.noreply.github.com>
2023-06-20 10:36:17 +0100

    Atualizei

--- apagarcargos.php
+++ apagarcargos.php
@@ -12,8 +12,8 @@ $Cod_cargo = filter_input(INPUT_GET, "Cod_cargo",FILTER_SANITIZE_NUMBER_INT);
         
 
         if($result_cargos->execute()){
-            header("Location: cargos.php");
             sleep(1);
+            header("Location: cargos.php");
             $msg="<div class='alert alert-success'>Cargo apagado Com sucesso!</div>";
             $retorna = ['erro' => false, 'msg' => "<div class='alert alert-success' role='alert' >Cargo apagado com sucesso! </div>"];
           
