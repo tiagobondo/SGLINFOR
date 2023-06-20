5f3c9098f9c94b6df89313b93033af6bed042ea4
Tiago Bondo <118920546+tiagobondo@users.noreply.github.com>
2023-06-20 14:18:52 +0100

    Atualizando

--- apagarcargos.php
+++ apagarcargos.php
@@ -11,7 +11,6 @@ $Cod_cargo = filter_input(INPUT_GET, "Cod_cargo",FILTER_SANITIZE_NUMBER_INT);
         $result_cargo = $pdo->prepare($query_cargo);
         $result_cargo->bindParam('Cod_cargo',$Cod_cargo);
         
-
         if($result_cargo->execute()){
             sleep(1);
             header("Location: cargos.php");
@@ -23,14 +22,14 @@ $Cod_cargo = filter_input(INPUT_GET, "Cod_cargo",FILTER_SANITIZE_NUMBER_INT);
         }
         else{
             $retorna = ['erro' => true, 'msg' => "<div class='alert alert-danger' role='alert' >Erro: Houve Um erro! </div>"];
-
+            header("Location: cargos.php");
         }
      }
 
     else{
         header("Location: cargos.php");
         $retorna = ['erro' => true, 'msg' => "<div class='alert alert-danger' role='alert' >Erro:Nenhum encontrado! </div>"];
-
+        header("Location: cargos.php");
     }
 
   
