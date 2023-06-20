66449a2e651f9aab1f73a2144fcf0071690c5f9a
Tiago Bondo <118920546+tiagobondo@users.noreply.github.com>
2023-06-20 21:27:49 +0100

    Atualizei a minha página list

--- list.php
+++ list.php
@@ -3,7 +3,7 @@
 session_start();
 
 include_once "conexao.php";
-    if($_SESSION['Cod_cargo'] =='Professor'){
+    if($_SESSION['Funcao'] =='Professor'){
 
     $pagina = filter_input(INPUT_GET,"pagina",FILTER_SANITIZE_NUMBER_INT);
 
