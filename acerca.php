b63ff867924057393b9d8a400ee3ba36efcdcbc1
Tiago Bondo <118920546+tiagobondo@users.noreply.github.com>
2023-06-20 21:10:25 +0100

    Atualizei o favicon na minha página

--- acerca.php
+++ acerca.php
@@ -9,6 +9,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Acerca</title>
+    <link rel="shortcut icon" href="./imagens//LogSGLINFOR.png" type="image/x-icon">
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="estilos/style.css">
     <link rel="stylesheet" href="estilos/icones/css/all.min.css">
