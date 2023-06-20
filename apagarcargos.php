3885d94e4c926e96ae6ff45a24fcad365c49a226
Tiago Bondo <118920546+tiagobondo@users.noreply.github.com>
2023-06-20 11:04:28 +0100

    Atualizado com sucesso

--- apagarcargos.php
+++ apagarcargos.php
@@ -2,35 +2,32 @@
 
 include_once "conexao.php";
 
+
 $Cod_cargo = filter_input(INPUT_GET, "Cod_cargo",FILTER_SANITIZE_NUMBER_INT);
-sleep(50);
 
 
     if(!empty($Cod_cargo)){
-        $query_cargos = "DELETE FROM cargos WHERE Cod_cargo=:Cod_cargo";
-        $result_cargos = $pdo->prepare($query_cargos);
-        $result_cargos->bindParam('Cod_cargo',$Cod_cargo);
+        $query_cargo = "DELETE FROM cargos WHERE Cod_cargo=:Cod_cargo";
+        $result_cargo = $pdo->prepare($query_cargo);
+        $result_cargo->bindParam('Cod_cargo',$Cod_cargo);
         
 
-        if($result_cargos->execute()){
-            
-            header("Location: cargos.php");
+        if($result_cargo->execute()){
+            sleep(1);
             $msg="<div class='alert alert-success'>Cargo apagado Com sucesso!</div>";
+            header("Location: cargos.php");
             $retorna = ['erro' => false, 'msg' => "<div class='alert alert-success' role='alert' >Cargo apagado com sucesso! </div>"];
-          
             
 
         }
         else{
             $retorna = ['erro' => true, 'msg' => "<div class='alert alert-danger' role='alert' >Erro: Houve Um erro! </div>"];
-           
 
         }
      }
 
     else{
         $retorna = ['erro' => true, 'msg' => "<div class='alert alert-danger' role='alert' >Erro:Nenhum encontrado! </div>"];
-        
 
     }
 
